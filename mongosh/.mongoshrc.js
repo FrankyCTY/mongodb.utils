e989add4df687c84b636deef503d853b781d8f89
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2022-06-06 16:55:45 +1200

    update start up script

--- mongosh/.mongoshrc.js
+++ mongosh/.mongoshrc.js
@@ -1,5 +1,6 @@
-console.log('welcome back Franky');
+console.log('Welcome Back Franky');
 
 scriptDir = '/home/tak/pdir/mongodb.utils/mongosh';
 
-load(`${scriptDir}/helpers.js`);
+// we just use .mongoshrc works as an entry point to the actual start up script
+load(`${scriptDir}/startup.js`);
--- mongosh/startup.js
+++ mongosh/startup.js
@@ -1,3 +1,7 @@
+// Use external editor - process "edit" to open
+// config.set('editor', 'code --wait');
+config.set('editor', 'vim');
+
 // Helpers function for some node api methods - https://www.mongodb.com/docs/manual/reference/method/js-native/#std-label-native-in-mongosh
 ls = (dirPath) => {
   if (!dirPath) return fs.readdirSync('.');
@@ -8,3 +12,4 @@ ls = (dirPath) => {
 pwd = () => process.cwd();
 
 db.getConnectionStatus = () => db.runCommand({ connectionStatus: 1 });
+db.getLastError = () => db.runCommand({ getLastError: 1 });
